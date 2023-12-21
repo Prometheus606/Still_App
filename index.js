f39c81d9560e9d3d289d39be9ee0e427150ba5b2
Niklas Beitler <niklas.beitler@duh-group.com>
2023-12-21 13:13:56 +0100

    added a time difference text and maked the code more clear

--- index.js
+++ index.js
@@ -2,30 +2,66 @@ const submitButton = document.getElementById("submit")
 const timeView = document.getElementById("time")
 const buttonR = document.getElementsByClassName("r")[0]
 const buttonL = document.getElementsByClassName("l")[0]
-const lastSideText = document.getElementById("last-side")
-const nextSideText = document.getElementById("next-side")
-const errorText = document.getElementById("error-text")
+const lastSideView = document.getElementById("last-side")
+const lastTimeView = document.getElementById("last-time")
+const timeDifferenceView = document.getElementById("time-difference")
+const nextSideView = document.getElementById("next-side")
+const errorView = document.getElementById("error-text")
 
 //localStorage.clear()
+
+submitButton.addEventListener("click", submit);
+buttonR.addEventListener("click", () => setSide(buttonR));
+buttonL.addEventListener("click", () => setSide(buttonL));
+
+
+let lastSideText = localStorage.getItem("lastSideText");
+let lastTimeText = localStorage.getItem("lastTimeText");
+let lastTime = localStorage.getItem("lastTime");
+let nextText = localStorage.getItem("nextText");
 
 let lastSide;
 let nextSide;
 let time;
+let timeDifference;
 
-submitButton.setAttribute("onclick", "submit()")
-buttonR.setAttribute("onclick", "setSideR()")
-buttonL.setAttribute("onclick", "setSideL()")
+if (lastSideText === null || nextText === null || lastTime === null) {
+    lastSideView.innerHTML = "Du hast noch keine Stillzeit eingetragen.";
+    nextSideView.innerHTML = "";
+    lastTimeView.innerHTML = "";
+    timeDifferenceView.innerHTML = "";
 
-
-let lastText = localStorage.getItem("lastText");
-let nextText = localStorage.getItem("nextText");
-
-if (lastText === null || nextText === null) {
-    lastSideText.textContent = "Du hast noch keine Stillzeit eingetragen.";
-    nextSideText.textContent = "";
 } else {
-    lastSideText.textContent = lastText;
-    nextSideText.textContent = nextText;
+    lastTime = {
+        hours: parseInt(lastTime.split(":")[0]),
+        minutes: parseInt(lastTime.split(":")[1])
+    }
+
+    calculateTimeDiffrence()
+
+    lastSideView.innerHTML = lastSideText;
+    lastTimeView.innerHTML = lastTimeText;
+    timeDifferenceView.innerHTML = "<strong>Differenz:</strong> " + timeDifference.hours + " Stunden, " + timeDifference.minutes + " Minuten";
+    nextSideView.innerHTML = nextText;
+}
+
+function calculateTimeDiffrence() {
+    let currentTime = {
+        hours: new Date().getHours(),
+        minutes: new Date().getMinutes(),
+    }
+
+    timeDifference = {
+        hours: currentTime.hours - lastTime.hours,
+        minutes: currentTime.minutes - lastTime.minutes,
+    };
+    if (timeDifference.hours < 0) {
+        timeDifference.hours += 24
+    }
+    if (timeDifference.minutes < 0) {
+        timeDifference.minutes += 60
+        timeDifference.hours -= 1
+    }
 }
 
 
@@ -44,7 +80,7 @@ function submit() {
 
     if (lastSide === undefined) {
         console.log("Keine Seite angegeben");
-        errorText.textContent = "Bitte eine Seite auswählen!"
+        errorView.textContent = "Bitte eine Seite auswählen!"
         return
     }
 
@@ -56,22 +92,30 @@ function submit() {
         nextSide = "Rechts"
     }
 
-    lastSideText.textContent = "Deine Letzte Stillzeit war um " + time + ", angefangen mit " + lastSide +".";
-    nextSideText.textContent = "Als nächstes anfangen mit " + nextSide + "!";
+    lastTime = {
+        hours: time.split(":")[0],
+        minutes: time.split(":")[1]
+    }
 
-    localStorage.setItem("lastText", lastSideText.textContent);
-    localStorage.setItem("nextText", nextSideText.textContent);
-}
+    calculateTimeDiffrence()
 
-function setSideR() {
-    setSide(buttonR)
-}
+    
+    lastSideView.innerHTML = "<strong>Letzte seite:</strong> " + lastSide
+    lastTimeView.innerHTML = "<strong>Letzte Stillzeit:</strong> " + time + " Uhr"
+    nextSideView.innerHTML = "Als nächstes anfangen mit: <br> <span>" + nextSide + "</span>"
+    timeDifferenceView.innerHTML = "<strong>Differenz:</strong> " + timeDifference.hours + " Stunden, " + timeDifference.minutes + " Minuten"
 
-function setSideL() {
-    setSide(buttonL)
+    localStorage.setItem("lastSideText", lastSideView.innerHTML);
+    localStorage.setItem("lastTime", time);
+    localStorage.setItem("lastTimeText", lastTimeView.innerHTML);
+    localStorage.setItem("nextText", nextSideView.innerHTML);
+
+    
+
 }
 
 function setSide(button) {
+    errorView.textContent = ""
     buttonL.style.border = "1px solid black";
     buttonR.style.border = "1px solid black";
     button.style.border = "2px solid rgb(206, 70, 70)";
